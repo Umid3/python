7e8e1fd4c4d0919c22471097be33e3f8a56fbcbc
umid <umiddear@gmail;com>
2023-11-25 15:16:51 +0500

    task3

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #Task 1
 # name = input("Enter your name: ")
 # age = input("Enter Your age: ")
-
+#############################################
 # Task 2 
-num = int(input("Enter a number: "))
-
-if num%2==0:
-    print(f" {num} is EVEN")
-else:
-    print(f" {num} is ODD")
+# num = int(input("Enter a number: "))
+# if num%2==0:
+#     print(f" {num} is EVEN")
+# else:
+#     print(f" {num} is ODD")
+##############################################
+#Task 3
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = []
+# for i in a:
+#     if i<5 :
+#         b.append(i)
+# print(b)
+#Task 4
